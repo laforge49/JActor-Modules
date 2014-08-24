e1e3ab0b25867e702246ccfe1154b215a60f9239
Bill La Forge <laforge49@gmail.com>
2014-08-24 13:02:04 -0400

    IntBlade -> MapBlade

---
+++ jactor2-common/src/test/java/org/agilewiki/jactor2/common/MapBladeTest.java
@@ -0,0 +1,64 @@
+package org.agilewiki.jactor2.common;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor2.core.blades.NonBlockingBladeBase;
+import org.agilewiki.jactor2.core.impl.Plant;
+import org.agilewiki.jactor2.core.requests.SOp;
+import org.agilewiki.jactor2.core.requests.impl.RequestImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapBladeTest extends TestCase {
+    public void test1() throws Exception {
+        new Plant();
+        try {
+            final MapBlade<String, Integer> mapBlade = new MapBlade();
+
+            new MapBlade.MapBladeSOp<Void, String, Integer>("initializeMapBlade", mapBlade) {
+                @Override
+                protected Void processSyncOperation(RequestImpl _requestImpl)
+                        throws Exception {
+                    Map<String, Integer> m = new HashMap();
+                    setMap(m);
+                    m.put("x", 42);
+                    return null;
+                }
+            }.call();
+
+            new MapBlade.MapBladeSOp<Void, String, Integer>("printX", mapBlade) {
+                @Override
+                protected Void processSyncOperation(RequestImpl _requestImpl)
+                        throws Exception {
+                    System.out.println(getMap().get("x"));
+                    return null;
+                }
+            }.call();
+        } finally {
+            Plant.close();
+        }
+    }
+}
+
+class MapBlade<K, V> extends NonBlockingBladeBase {
+    private Map<K, V> m;
+
+    public MapBlade() throws Exception {}
+
+    public static abstract class MapBladeSOp<RESPONSE_TYPE, K, V> extends SOp<RESPONSE_TYPE> {
+        protected final MapBlade<K, V> mapBlade;
+
+        public MapBladeSOp(String _opName, MapBlade<K, V> _mapBlade) {
+            super(_opName, _mapBlade.getReactor());
+            mapBlade = _mapBlade;
+        }
+
+        protected Map<K, V> getMap() {
+            return mapBlade.m;
+        }
+
+        protected void setMap(Map<K, V> _m) {
+            mapBlade.m = _m;
+        }
+    }
+}

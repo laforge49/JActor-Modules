687aec2a5734ff513f260c0a90e5b531dd0cc3c3
Bill La Forge <laforge49@gmail.com>
2014-09-19 09:17:46 +0530

    #19 CFacility supports widget factories

--- jactor2-common/src/main/java/org/agilewiki/jactor2/common/CFacility.java
+++ jactor2-common/src/main/java/org/agilewiki/jactor2/common/CFacility.java
@@ -1,10 +1,19 @@
 package org.agilewiki.jactor2.common;
 
 import org.agilewiki.jactor2.common.services.ClassLoaderService;
+import org.agilewiki.jactor2.common.widgets.WidgetFactory;
+import org.agilewiki.jactor2.core.blades.ismTransactions.ISMap;
+import org.agilewiki.jactor2.core.plant.PlantBase;
 import org.agilewiki.jactor2.core.reactors.Facility;
+import org.agilewiki.jactor2.core.requests.SOp;
+import org.agilewiki.jactor2.core.requests.impl.RequestImpl;
 import org.xeustechnologies.jcl.JarClassLoader;
 
+import java.lang.reflect.Constructor;
+
 public class CFacility extends Facility {
+    protected ISMap<WidgetFactory> widgetFactories = PlantBase.createISMap();
+
     public CFacility(String _name) throws Exception {
         super(_name);
     }
@@ -31,4 +40,49 @@ public class CFacility extends Facility {
             return jcl;
         return ClassLoader.getSystemClassLoader();
     }
+
+    public Class loadClass(String _className) throws Exception {
+        return getClassLoader().loadClass(_className);
+    }
+
+    public ISMap<WidgetFactory> getWidgetFactories() {
+        return widgetFactories;
+    }
+
+    public WidgetFactory getWidgetFactory(final String _name) {
+        return widgetFactories.get(_name);
+    }
+
+    public boolean hasWidgetFactory(final String _name) {
+        return widgetFactories.containsKey(_name);
+    }
+
+    public SOp<Void> addWidgetFactorySOp(final WidgetFactory _widgetFactory) {
+        final String _name = _widgetFactory.getName();
+        if (_name == null) {
+            throw new IllegalArgumentException("name may not be null");
+        }
+        if (_name.length() == 0) {
+            throw new IllegalArgumentException("name may not be empty");
+        }
+        if (_name.contains(" ")) {
+            throw new IllegalArgumentException("name may not contain spaces: "
+                    + _name);
+        }
+        if (_name.contains(".")) {
+            throw new IllegalArgumentException("name may not contain .: "
+                    + _name);
+        }
+        return new SOp<Void>("addWidgetFactory", this) {
+            @Override
+            protected Void processSyncOperation(RequestImpl _requestImpl) throws Exception {
+                if (hasWidgetFactory(_name)) {
+                    throw new IllegalArgumentException("duplicate widget factory name");
+                }
+                widgetFactories = widgetFactories.plus(_name, _widgetFactory);
+                return null;
+            }
+        };
+    }
 }
+

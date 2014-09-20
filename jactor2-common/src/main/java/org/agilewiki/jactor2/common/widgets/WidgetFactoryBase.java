9998340019b6c2c36c515e3992e5eda97b0ee469
Bill La Forge <laforge49@gmail.com>
2014-09-20 10:00:58 +0530

    #19 WidgetBase nested in WidgetFactoryBase

--- jactor2-common/src/main/java/org/agilewiki/jactor2/common/widgets/WidgetFactoryBase.java
+++ jactor2-common/src/main/java/org/agilewiki/jactor2/common/widgets/WidgetFactoryBase.java
@@ -3,8 +3,9 @@ package org.agilewiki.jactor2.common.widgets;
 import org.agilewiki.jactor2.common.CFacility;
 import org.agilewiki.jactor2.core.blades.BladeBase;
 import org.agilewiki.jactor2.core.blades.NamedBlade;
+import org.agilewiki.jactor2.core.reactors.Reactor;
 
-public abstract class WidgetFactoryBase extends BladeBase implements WidgetFactory, NamedBlade {
+public class WidgetFactoryBase extends BladeBase implements WidgetFactory, NamedBlade {
     public static String factoryKey(String facilityName, String widgetFactoryName) {
         return facilityName + "." + widgetFactoryName;
     }
@@ -28,5 +29,28 @@ public abstract class WidgetFactoryBase extends BladeBase implements WidgetFacto
     @Override
     public String getFactoryKey() {
         return factoryKey(getCFacility().name, name);
+    }
+
+    public Widget newWidget(Reactor _reactor, Widget _parentWidget) throws Exception {
+        return new WidgetBase(_reactor, _parentWidget);
+    }
+
+    protected class WidgetBase extends BladeBase implements Widget {
+        private final Widget widgetParent;
+
+        WidgetBase(Reactor _reactor, Widget _widgetParent) {
+            widgetParent = _widgetParent;
+            _initialize(_reactor);
+        }
+
+        @Override
+        public WidgetFactory getWidgetFactory() {
+            return WidgetFactoryBase.this;
+        }
+
+        @Override
+        public Widget getParentWidget() {
+            return widgetParent;
+        }
     }
 }

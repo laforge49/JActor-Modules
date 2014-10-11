66983942e9febf24ca01edb9eda704f85b75697b
Bill La Forge <laforge49@gmail.com>
2014-10-11 19:33:34 +0530

    #24 TransmutableByteBuffer, UnmodifiableByteBufferFactory

---
+++ jactor2-durable/src/main/java/org/agilewiki/jactor2/durable/TransmutableByteBuffer.java
@@ -0,0 +1,34 @@
+package org.agilewiki.jactor2.durable;
+
+import org.agilewiki.jactor2.core.blades.transmutable.Transmutable;
+
+import java.nio.ByteBuffer;
+
+/**
+ * A transmutable wrapper of a byte buffer.
+ */
+public class TransmutableByteBuffer implements Transmutable<UnmodifiableByteBufferFactory> {
+    public final ByteBuffer byteBuffer;
+
+    public TransmutableByteBuffer(final byte[] _bytes) {
+        byteBuffer = ByteBuffer.wrap(_bytes);
+    }
+
+    public TransmutableByteBuffer(final int _length) {
+        byteBuffer = ByteBuffer.allocate(_length);
+    }
+
+    public TransmutableByteBuffer(final ByteBuffer _byteBuffer) {
+        byteBuffer = _byteBuffer;
+    }
+
+    @Override
+    public UnmodifiableByteBufferFactory createUnmodifiable() {
+        return new UnmodifiableByteBufferFactory(byteBuffer.array());
+    }
+
+    @Override
+    public Transmutable recreate(UnmodifiableByteBufferFactory unmodifiable) {
+        return unmodifiable.transmutableByteBuffer();
+    }
+}
